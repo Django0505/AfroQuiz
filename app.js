e2ab3f40a4b71a8280009285a9ffe31da3907169
emanuel281 <emanuel@projectcodex.co>
2015-05-07 15:48:46 +0200

    Added options property to json objects

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 var express = require('express'),
-    exphbs  = require('express-handlebars');
+    exphbs  = require('express-handlebars'),
+    bodyParser = require("body-parser");
 
 var app = express();
+
+var countries  = {
+	"uganda" : {
+
+		}
+
+};
+
 
 app.engine("handlebars", exphbs({defaultLayout:"main"}));
 app.set("view engine", "handlebars");
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
 app.use("/static", express.static("views"));
 app.use("/static", express.static("."));
+
+var countriesMap = {
+	'uganda' : require("./uganda_questions.json"),
+
+}
 
 app.get("/", function(req, res){
 	res.render("home")
@@ -17,12 +36,19 @@ app.get("/learn", function (req, res) {
 	res.render("learn");
 });
 
+app.get('/quiz/:country_name', function(){
+	var country_name = req.params.country_name
+});
+
 app.get("/uganda_quiz", function(req, res){
 	var uganda_quiz = require("./uganda_questions.json");
+
 	res.render("uganda_quiz", {uganda_quiz:uganda_quiz});
 });
 
 app.post("/uganda_quiz/uganda_quiz_report", function(req, res){
+	var input = JSON.parse(JSON.stringify(req.body));
+	console.log(req.body)
 	res.redirect("/");
 })
 
