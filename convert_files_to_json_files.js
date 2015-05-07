1f08a7b8b99177d05c24fe0930cd5350dca90c4b
emanuel281 <emanuel@projectcodex.co>
2015-05-07 10:51:46 +0200

    converted CSV qustion files to json files

---
+++ convert_files_to_json_files.js
@@ -0,0 +1,26 @@
+var fs = require("fs");
+
+convert_question_csv_to_json = function (filename, filename2) {
+	var read_file = fs.readFileSync(filename, "utf-8");
+	var regexp = /\r?\n/;
+	var file_rows = read_file.split(regexp);
+	var file_row_cols = file_rows.map(function(row){
+		var line = row.split(",");
+		return {
+			question: line[0],
+			answer: line[1],
+			country: line[2]
+		}
+	});
+
+	var to_be_json = JSON.stringify(file_row_cols);
+
+	fs.writeFileSync(filename2, to_be_json);
+	console.log(to_be_json)
+}
+
+convert_question_csv_to_json("./uganda_questions.csv", "uganda_questions.json");
+convert_question_csv_to_json("./chad_questions.csv", "chad_questions.json");
+convert_question_csv_to_json("./ethiopia_questions.csv", "ethiopia_questions.json");
+convert_question_csv_to_json("./nigeria_questions.csv", "nigeria_questions.json");
+convert_question_csv_to_json("./senegal_questions.csv", "senegal_questions.json");
