65651212cfdea46de264166dcc7d32e8e6f1d9e9
sibura <sibusiso@projectcodex.co>
2015-05-08 10:23:29 +0200

    Afro Quize modified

--- app.js
+++ app.js
@@ -52,6 +52,38 @@ app.post("/uganda_quiz/uganda_quiz_report", function(req, res){
 	res.redirect("/uganda_quiz");
 })
 
+app.post("/senegal_quiz/senegal_quiz_report", function(req, res){
+
+	var countries = countriesMap["senegal_quiz"];
+	var answers = {
+		Question_1 : "Name 3 languages spoken in Senegal?",
+		answers_1 = ["French;Wolof;Soninke"],
+		Question_2 : "What are  three border countries of Senegal?",
+		answers_2 = ["Gambia","Guinea-Bissau","Mali"],
+		Question_3 : "What is Senegal's currency?",
+		answers_3 =  "CFA franc(XOF)",
+		Question_4 : "What is the capital town of Senegal?",
+		answers_4 =  "Dakar",
+		Question_5 : "What are the two largest cities in Senegal?",
+		answers_5 = ["Dakar","Grand Dakar"],
+		Question_6 : "What is the population count?",
+		answers_6 =  "13,6 million",
+		Question_7 : "What is Senegal's growth rate?",
+		answers_7 =  "4%",
+		Question_8 : "What is the percentage of Islam?",
+		answers_8 =  "90%",
+		Question_9 : "What is their internet code?",
+		answers_9 =  ".sn"
+	}
+
+	countries.forEach(function(country, i){
+		var k = 1+i;
+		console.log(k)
+	});
+
+	res.redirect("/senegal_quiz");
+})
+
 app.get("/chad_quiz", function(req, res){
 	res.render("chad_quiz", {chad_quiz:countriesMap["chad_quiz"]});
 });
